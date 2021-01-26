5246a418c8f3342190c3df6d00ff24131d88d02f
Farzin Mirzaie <farzin@firstsource.tech>
2021-01-26 17:09:08 +0330

    Debugging

--- packages/update_checker/lib/src/bloc/update_bloc.dart
+++ packages/update_checker/lib/src/bloc/update_bloc.dart
@@ -76,7 +76,7 @@ class UpdateBloc extends Bloc<UpdateEvent, UpdateState> {
     bool isOptional,
     int buildNumber,
   ) {
-    final Map<String, String> changelogs = jsonDecode(
+    final Map<String, dynamic> changelogs = json.decode(
       // rawUpdateInfo['changelogs'].toString().replaceAll(r'\n', r'\\n'),
       r'{"en":"- Feature 1\n- Feature 2","es":"- característica 1\n- característica 2"}',
     );
