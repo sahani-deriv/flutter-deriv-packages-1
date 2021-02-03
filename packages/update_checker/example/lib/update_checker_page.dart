de504d7e184fce600c3ac457f3ca3bf5febccb3e
Farzin Mirzaie <farzin@firstsource.tech>
2021-02-03 18:24:59 +0330

    Update example

--- packages/update_checker/example/lib/update_checker_page.dart
+++ packages/update_checker/example/lib/update_checker_page.dart
@@ -2,26 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:update_checker/update_checker.dart';
 
 /// UpdateCheckerPage
-class UpdateCheckerPage extends StatefulWidget {
-  @override
-  _UpdateCheckerPageState createState() => _UpdateCheckerPageState();
-}
-
-class _UpdateCheckerPageState extends State<UpdateCheckerPage> {
-  UpdateState state;
-
+class UpdateCheckerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: const Text('UpdateChecker'),
         ),
         body: UpdateCheckerProvider(
-          UpdateChecker(
-            onStateChange: (UpdateState state) => setState(() {
-              this.state = state;
-            }),
-            child: Center(child: Text('${state.runtimeType}')),
+          Builder(
+            builder: (BuildContext context) => UpdateChecker(
+              onNotAvailable: () => _showSnackBar(
+                context,
+                'Update not available',
+              ),
+              onAvailable: (_) => _showSnackBar(
+                context,
+                'Update available!',
+              ),
+              child: const Center(
+                child: Text('The result will be shown in the snack bar'),
+              ),
+            ),
           ),
         ),
       );
+
+  void _showSnackBar(BuildContext context, String message) {
+    Scaffold.of(context).showSnackBar(SnackBar(content: Text(message)));
+  }
 }
