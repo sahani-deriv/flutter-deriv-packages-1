9b6795dd09f13b06ed6a3e31524f4fafa475d4b6
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-03-01 14:20:31 +0330

    Export PSAR indicator file

--- packages/deriv_technical_analysis/lib/deriv_technical_analysis.dart
+++ packages/deriv_technical_analysis/lib/deriv_technical_analysis.dart
@@ -25,6 +25,7 @@ export 'src/indicators/calculations/ma_env/ma_env_upper_indicator.dart';
 export 'src/indicators/calculations/macd/macd_histogram_indicator.dart';
 export 'src/indicators/calculations/macd/macd_indicator.dart';
 export 'src/indicators/calculations/macd/signal_macd_indicator.dart';
+export 'src/indicators/calculations/parabolic_sar.dart';
 export 'src/indicators/calculations/sma_indicator.dart';
 export 'src/indicators/calculations/statistics/standard_deviation_indicator.dart';
 export 'src/indicators/calculations/statistics/variance_indicator.dart';
