f8f75c2a0d20a87eede595f42f8fe07d1a7bd9c9
Samin-FS <samin@firstsource.tech>
2021-03-17 18:16:49 +0330

    fix tests using low/high

--- packages/deriv_technical_analysis/test/indicators/calculations/zigzag_indicator_test.dart
+++ packages/deriv_technical_analysis/test/indicators/calculations/zigzag_indicator_test.dart
@@ -29,10 +29,8 @@ void main() {
           ZigZagIndicator<MockResult>(MockInput(ticks), 1);
 
       expect(indicator.getValue(0).quote.isNaN, true);
-      expect(indicator.getValue(1).quote, 65.79);
-      expect(indicator.getValue(2).quote.isNaN, true);
-      expect(indicator.getValue(3).quote.isNaN, true);
-      expect(indicator.getValue(5).quote, 65);
+      expect(indicator.getValue(1).quote,68);
+      expect(indicator.getValue(5).quote.isNaN, true);
       expect(indicator.getValue(10).quote, 60);
       expect(indicator.getValue(11).quote, 62.51);
     });
