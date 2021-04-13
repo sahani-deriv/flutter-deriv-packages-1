5180e943910f24bf3a4ab4974bc2735df38d6da9
Samin-FS <samin@firstsource.tech>
2021-04-13 17:18:06 +0430

    revert unwanted changes

--- packages/deriv_technical_analysis/lib/src/indicators/cached_indicator.dart
+++ packages/deriv_technical_analysis/lib/src/indicators/cached_indicator.dart
@@ -43,7 +43,7 @@ abstract class CachedIndicator<T extends IndicatorResult> extends Indicator<T> {
 
   @override
   T getValue(int index) {
-    growResultsForIndex(index);
+    _growResultsForIndex(index);
 
     if (results[index] == null) {
       results[index] = calculate(index);
@@ -59,7 +59,7 @@ abstract class CachedIndicator<T extends IndicatorResult> extends Indicator<T> {
   /// Grows the results list with null elements to cover the given [index].
   ///
   /// Returns number of elements that have been added to the results list.
-  int growResultsForIndex(int index) {
+  int _growResultsForIndex(int index) {
     final int oldResultsCount = results.length;
 
     if (index > oldResultsCount - 1) {
@@ -70,13 +70,13 @@ abstract class CachedIndicator<T extends IndicatorResult> extends Indicator<T> {
   }
 
   /// Calculates the value of this indicator for the given [index] without caching it.
-  ///
+  /// 
   /// Returns the result as a [T].
   T calculate(int index);
 
   /// Invalidates a calculated indicator value for [index]
   void invalidate(int index) {
-    growResultsForIndex(index);
+    _growResultsForIndex(index);
     results[index] = null;
   }
 
