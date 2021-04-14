162e8d46215d02cbf8eee46b6d11f0cf4100232f
Samin-FS <samin@firstsource.tech>
2021-04-14 16:02:31 +0430

    add gator indicator nad tests

---
+++ packages/deriv_technical_analysis/lib/src/indicators/calculations/gator/gator_oscillator_bottom_indicator.dart
@@ -0,0 +1,55 @@
+import 'dart:math';
+
+import 'package:deriv_technical_analysis/src/indicators/cached_indicator.dart';
+import 'package:deriv_technical_analysis/src/indicators/calculations/mma_indicator.dart';
+import 'package:deriv_technical_analysis/src/indicators/indicator.dart';
+import 'package:deriv_technical_analysis/src/models/models.dart';
+
+///Gator Oscillator Indicator Bottom bar
+class GatorOscillatorIndicatorBottomBar<T extends IndicatorResult>
+    extends CachedIndicator<T> {
+  /// Initializes
+  GatorOscillatorIndicatorBottomBar(Indicator<T> indicator,
+      {MMAIndicator<T> lipsIndicator,
+      int lipsPeriod = 5,
+      this.lipsOffset = 3,
+      MMAIndicator<T> teethIndicator,
+      int teethPeriod = 8,
+      this.teethOffset = 5})
+      : _lipsIndicator =
+            lipsIndicator ?? MMAIndicator<T>(indicator, lipsPeriod),
+        _teethIndicator =
+            teethIndicator ?? MMAIndicator<T>(indicator, teethPeriod),
+        super.fromIndicator(indicator);
+
+  ///Offset of lips
+  final int lipsOffset;
+
+  ///Offset of teeth
+  final int teethOffset;
+
+  ///Lips (middle smoothed moving average) indicator
+  final MMAIndicator<T> _lipsIndicator;
+
+  ///Teeth (upper smoothed moving average) indicator
+  final MMAIndicator<T> _teethIndicator;
+
+  @override
+  T calculate(int index) {
+    final int maxOffset = max(lipsOffset, teethOffset);
+
+    if (index < maxOffset) {
+      return createResult(index: index, quote: double.nan);
+    }
+
+    final IndicatorResult lipsIndicator =
+        _lipsIndicator.getValue(index - lipsOffset);
+    final IndicatorResult teethIndicator =
+        _teethIndicator.getValue(index - teethOffset);
+
+    ///GatorBottomBar = – (Absolute value of (Teeth – Lips))
+    final double _quote = -(teethIndicator.quote - lipsIndicator.quote).abs();
+
+    return createResult(index: index, quote: _quote);
+  }
+}
---
+++ packages/deriv_technical_analysis/lib/src/indicators/calculations/gator/gator_oscillator_top_indicator.dart
@@ -0,0 +1,54 @@
+import 'dart:math';
+
+import 'package:deriv_technical_analysis/src/indicators/cached_indicator.dart';
+import 'package:deriv_technical_analysis/src/indicators/calculations/mma_indicator.dart';
+import 'package:deriv_technical_analysis/src/indicators/indicator.dart';
+import 'package:deriv_technical_analysis/src/models/models.dart';
+
+///Gator Oscillator Indicator Top bar
+class GatorOscillatorIndicatorTopBar<T extends IndicatorResult>
+    extends CachedIndicator<T> {
+  /// Initializes
+  GatorOscillatorIndicatorTopBar(Indicator<T> indicator,
+      {MMAIndicator<T> jawIndicator,
+      int jawPeriod = 13,
+      this.jawOffset = 8,
+      MMAIndicator<T> teethIndicator,
+      int teethPeriod = 8,
+      this.teethOffset = 5})
+      : _jawIndicator = jawIndicator ?? MMAIndicator<T>(indicator, jawPeriod),
+        _teethIndicator =
+            teethIndicator ?? MMAIndicator<T>(indicator, teethPeriod),
+        super.fromIndicator(indicator);
+
+  ///Jaw (lower smoothed moving average) indicator
+  final MMAIndicator<T> _jawIndicator;
+
+  ///Offset of jaw
+  final int jawOffset;
+
+  ///Offset of teeth
+  final int teethOffset;
+
+  ///Teeth (upper smoothed moving average) indicator
+  final MMAIndicator<T> _teethIndicator;
+
+  @override
+  T calculate(int index) {
+    final int maxOffset = max(jawOffset, teethOffset);
+
+    if (index < maxOffset) {
+      return createResult(index: index, quote: double.nan);
+    }
+
+    final IndicatorResult jawIndicator =
+        _jawIndicator.getValue(index - jawOffset);
+    final IndicatorResult teethIndicator =
+        _teethIndicator.getValue(index - teethOffset);
+
+    ///GatorTopBar = Absolute value of(Jaw - Teeth)
+    final double _quote = (jawIndicator.quote - teethIndicator.quote).abs();
+
+    return createResult(index: index, quote: _quote);
+  }
+}
