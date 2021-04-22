da15bc7fea811f8f86e0f1769a0c8bbf2d2cf0ab
Suyash Mohan <suyash@binary.com>
2021-04-22 14:19:18 +0800

    rename path to presentation

--- packages/update_checker/lib/update_checker.dart
+++ packages/update_checker/lib/update_checker.dart
@@ -2,5 +2,5 @@ library update_checker;
 
 export 'src/bloc/update_bloc.dart';
 export 'src/models/models.dart';
+export 'src/presentation/update_checker.dart';
 export 'src/repositories/repositories.dart';
-export 'src/view/update_checker.dart';
