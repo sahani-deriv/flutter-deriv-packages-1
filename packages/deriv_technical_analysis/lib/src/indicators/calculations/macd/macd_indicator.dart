59db67413298f3ae313bcf37507e5a5614eeab1f
mohammadamir-fs <mohammadamir@firstsource.tech>
2021-06-13 13:38:42 +0430

    fix: fix typos and names

--- packages/deriv_technical_analysis/lib/src/indicators/calculations/macd/macd_indicator.dart
+++ packages/deriv_technical_analysis/lib/src/indicators/calculations/macd/macd_indicator.dart
@@ -1,17 +1,16 @@
 import 'package:deriv_technical_analysis/deriv_technical_analysis.dart';
 
-// TODO(mohammadamir-fs): test macd visually.
-/// Moving Average Convergance Divergence Indicator.
+/// Moving Average Convergence Divergence Indicator.
 class MACDIndicator<T extends IndicatorResult> extends CachedIndicator<T> {
-  /// Creates a  Moving average convergance divergance indicator from a given indicator,
+  // / Creates a  Moving average convergence divergence indicator from a given indicator,
   /// with short term ema set to `12` periods and long term ema set to `26` periods as default.
   MACDIndicator.fromIndicator(
     Indicator<T> indicator, {
-    int shortBarcount = 12,
-    int longBarCount = 26,
-  })  : assert(shortBarcount < longBarCount),
-        _shortTermEma = EMAIndicator<T>(indicator, shortBarcount),
-        _longTermEma = EMAIndicator<T>(indicator, longBarCount),
+    int fastMAPeriod = 12,
+    int slowMAPeriod = 26,
+  })  : assert(fastMAPeriod < slowMAPeriod),
+        _shortTermEma = EMAIndicator<T>(indicator, fastMAPeriod),
+        _longTermEma = EMAIndicator<T>(indicator, slowMAPeriod),
         super.fromIndicator(indicator);
 
   final EMAIndicator<T> _shortTermEma;
