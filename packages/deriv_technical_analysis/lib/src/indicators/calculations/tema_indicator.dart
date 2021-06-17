c6009b24090d170e254a7ba5af513a12b921ce10
Ramin Vakili <ramin.vakili@firstsource.tech>
2021-06-17 09:09:55 +0430

    Use factory constructor for TEMAIndicator

--- packages/deriv_technical_analysis/lib/src/indicators/calculations/tema_indicator.dart
+++ packages/deriv_technical_analysis/lib/src/indicators/calculations/tema_indicator.dart
@@ -7,21 +7,20 @@ import 'ema_indicator.dart';
 /// Triple EMA indicator.
 class TEMAIndicator<T extends IndicatorResult> extends CachedIndicator<T> {
   /// Initializes
-  TEMAIndicator(Indicator<T> indicator, int period)
-      : this._first(indicator, EMAIndicator<T>(indicator, period), period);
+  factory TEMAIndicator(Indicator<T> indicator, int period) {
+    final EMAIndicator<T> ema = EMAIndicator<T>(indicator, period);
+    final EMAIndicator<T> emaEma = EMAIndicator<T>(ema, period);
+    final EMAIndicator<T> emaEmaEma = EMAIndicator<T>(emaEma, period);
+    return TEMAIndicator<T>._(indicator, ema, emaEma, emaEmaEma, period);
+  }
 
-  TEMAIndicator._first(Indicator<T> indicator, EMAIndicator<T> ema, int period)
-      : this._second(indicator, ema, EMAIndicator<T>(ema, period), period);
-
-  TEMAIndicator._second(
+  TEMAIndicator._(
     Indicator<T> indicator,
-    EMAIndicator<T> ema,
-    EMAIndicator<T> emaEma,
+    this._ema,
+    this._emaEma,
+    this._emaEmaEma,
     this.period,
-  )   : _ema = ema,
-        _emaEma = emaEma,
-        _emaEmaEma = EMAIndicator<T>(emaEma, period),
-        super.fromIndicator(indicator);
+  ) : super.fromIndicator(indicator);
 
   /// The period.
   final int period;
