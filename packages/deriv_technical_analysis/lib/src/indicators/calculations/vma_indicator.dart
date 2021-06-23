ae44e2d55d2d4055d0b3645faa3b46f12cdc4f15
Samin-FS <samin@firstsource.tech>
2021-06-23 15:29:57 +0430

    WIP-TESTING

---
+++ packages/deriv_technical_analysis/lib/src/indicators/calculations/vma_indicator.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+import 'package:deriv_technical_analysis/src/indicators/calculations/cmo_indicator.dart';
+import 'package:deriv_technical_analysis/src/models/models.dart';
+
+import '../cached_indicator.dart';
+import '../indicator.dart';
+
+/// Variable Moving Average Indicator
+class VMAIndicator<T extends IndicatorResult> extends CachedIndicator<T> {
+  /// Initializes
+  VMAIndicator(this.indicator, this.period)
+      : cmo = CMOIndicator<T>(indicator, period),
+        super.fromIndicator(indicator);
+
+  /// Indicator to calculate SMA on
+  final Indicator<T> indicator;
+  final Indicator<T> cmo;
+
+  /// Period
+  final int period;
+
+  @override
+  T calculate(int index) {
+    final int realBarCount = min(period, index + 1);
+    final double result = 2 /
+            (1 + realBarCount) *
+            indicator.getValue(index).quote *
+            (indicator.getValue(index).quote - getValue(index - 1).quote) +
+        getValue(index - 1).quote;
+    return createResult(index: index, quote: result);
+  }
+}
