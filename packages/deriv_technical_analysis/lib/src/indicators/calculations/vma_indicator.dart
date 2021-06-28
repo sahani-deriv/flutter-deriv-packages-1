d94c247c33223abd50d7ccc2600a610a48ecb7df
Samin-FS <samin@firstsource.tech>
2021-06-28 11:59:25 +0430

    WIP

--- packages/deriv_technical_analysis/lib/src/indicators/calculations/vma_indicator.dart
+++ packages/deriv_technical_analysis/lib/src/indicators/calculations/vma_indicator.dart
@@ -11,240 +11,29 @@ import '../indicator.dart';
 class VMAIndicator<T extends IndicatorResult> extends CachedIndicator<T> {
   /// Initializes
   VMAIndicator(this.indicator, this.period)
-      : pdmS = PdmS<T>(indicator, 1 / period),
-        mdmS = MdmS<T>(indicator, 1 / period),
-        pdiS = PdiS<T>(indicator, 1 / period),
-        mdiS = MdiS<T>(indicator, 1 / period),
-        iS = IS<T>(indicator, 1 / period)..calculateValues(),
+      :cmo=CMOIndicator<T>(indicator, 9),
         super.fromIndicator(indicator);
 
   /// Indicator to calculate SMA on
   final Indicator<T> indicator;
 
-  ///
-  final CachedIndicator<T> pdmS;
-
-  ///
-  final CachedIndicator<T> mdmS;
-
-  ///
-  final CachedIndicator<T> pdiS;
-
-  ///
-  final CachedIndicator<T> mdiS;
-
-  ///
-  final CachedIndicator<T> iS;
-
   /// Chande Momentum Oscillator indicator
-  // final Indicator<T> cmo;
+  final Indicator<T> cmo;
 
   /// Period
   final int period;
 
   @override
   T calculate(int index) {
-    if(index<1){
-      return createResult(
-          index: index,
-          quote:0);
-          }
-    final double hhv =
-        HighestValueIndicator<T>(iS, period).getValue(index).quote;
-    final double llv =
-        LowestValueIndicator<T>(iS, period).getValue(index).quote;
 
-    return createResult(
-        index: index,
-        quote: ((1 - (1 / period)) *
-                    (iS.getValue(index).quote - llv) /
-                    (hhv - llv)) *
-                (index < 1 ? 0 : getValue(index - 1).quote) +
-            indicator.getValue(index).quote *
-                (1 / period) *
-                (iS.getValue(index).quote - llv) /
-                (hhv - llv));
-
-    //
-    // if (index <= 1) {
-    //   return createResult(index: index, quote: indicator.getValue(index).quote);
-    // }
-    // // final int realBarCount = min(period, index + 1);
-    // final double factor = 2 / (period + 1);
-    // final double result =
-    //     (factor * cmo.getValue(index).quote * indicator.getValue(index).quote) +
-    //         ((1 - factor * cmo.getValue(index).quote) * getValue(1).quote);
-    //
-    // return createResult(index: index, quote: result);
-  }
-}
-
-/// Variable Moving Average Indicator
-class PdmS<T extends IndicatorResult> extends CachedIndicator<T> {
-  ///
-  PdmS(this.indicator, this.period) : super.fromIndicator(indicator);
-
-  /// Period
-  final double period;
-
-  /// Indicator to calculate
-  final Indicator<T> indicator;
-
-  @override
-  T calculate(int index) {
-    print(index);
-    if(index<1){
-      print("Aaaa");
-      return createResult(
-          index: index,
-          quote:0);
+    final double a=2/(period+1);
+    final double b=(cmo.getValue(index).quote/100).abs();
+    double sum=0;
+    double k=0;
+    for (int i = max(0, index - period + 1); i <= index; i++) {
+      sum += indicator.getValue(i).quote* pow(a*b,i);
+      k+=pow(a*b,i);
     }
-    final double pdm = index < 1
-        ? 0
-        : max(
-            indicator.getValue(index).quote -
-                indicator.getValue(index - 1).quote,
-            0);
-    return createResult(
-        index: index,
-        quote: (1 - period) *
-            ((index < 1 ? 0 : getValue(index - 1).quote) +
-                period * (pdm.isNaN ? 0 : pdm)));
-  }
-}
-
-/// Variable Moving Average Indicator
-class MdmS<T extends IndicatorResult> extends CachedIndicator<T> {
-  ///
-  MdmS(this.indicator, this.period) : super.fromIndicator(indicator);
-
-  /// Period
-  final double period;
-
-  /// Indicator to calculate
-  final Indicator<T> indicator;
-
-  @override
-  T calculate(int index) {
-    if(index<1){
-      return createResult(
-          index: index,
-          quote:0);
-    }
-    final double mdm = index < 1
-        ? 0
-        : max(
-            indicator.getValue(index - 1).quote -
-                indicator.getValue(index).quote,
-            0);
-    return createResult(
-        index: index,
-        quote: (1 - period) * (index < 1 ? 0 : getValue(index - 1).quote) +
-            period * (mdm.isNaN ? 0 : mdm));
-  }
-}
-
-/// Variable Moving Average Indicator
-class PdiS<T extends IndicatorResult> extends CachedIndicator<T> {
-  ///
-  PdiS(Indicator<T> indicator, this.period)
-      : mdmS = MdmS<T>(indicator, period)..calculateValues(),
-        pdmS = PdmS<T>(indicator, period)..calculateValues(),
-        super.fromIndicator(indicator);
-
-  /// Period
-  final double period;
-
-  /// Indicator to calculate
-  final Indicator<T> pdmS;
-
-  ///
-  final Indicator<T> mdmS;
-
-  @override
-  T calculate(int index) {
-    if(index<1){
-      print("qqqqq");
-      return createResult(
-          index: index,
-          quote:0);
-    }
-    final double pdi = index < 1
-        ? 0
-        : pdmS.getValue(index).quote /
-            (pdmS.getValue(index).quote + mdmS.getValue(index).quote);
-    return createResult(
-        index: index,
-        quote: (1 - period) * (index < 1 ? 0 : getValue(index - 1).quote) +
-            period * (pdi.isNaN ? 0 : pdi));
-  }
-}
-
-/// Variable Moving Average Indicator
-class MdiS<T extends IndicatorResult> extends CachedIndicator<T> {
-  ///
-  MdiS(Indicator<T> indicator, this.period)
-      : mdmS = MdmS<T>(indicator, period)..calculateValues(),
-        pdmS = PdmS<T>(indicator, period)..calculateValues(),
-        super.fromIndicator(indicator);
-
-  /// Period
-  final double period;
-
-  /// Indicator to calculate
-  final Indicator<T> pdmS;
-
-  ///
-  final Indicator<T> mdmS;
-
-  @override
-  T calculate(int index) {
-    if(index<1){
-      return createResult(
-          index: index,
-          quote:0);
-    }
-    final double mdi = index < 1
-        ? 0
-        : mdmS.getValue(index).quote /
-            (pdmS.getValue(index).quote + mdmS.getValue(index).quote);
-    return createResult(
-        index: index,
-        quote: (1 - period) * (index < 1 ? 0 : getValue(index - 1).quote) +
-            period * (mdi.isNaN ? 0 : mdi));
-  }
-}
-
-/// Variable Moving Average Indicator
-class IS<T extends IndicatorResult> extends CachedIndicator<T> {
-  ///
-  IS(Indicator<T> indicator, this.period)
-      : mdiS = MdiS<T>(indicator, period),
-        pdiS = PdiS<T>(indicator, period),
-        super.fromIndicator(indicator);
-
-  /// Period
-  final double period;
-
-  /// Indicator to calculate
-
-  final Indicator<T> pdiS;
-
-  ///
-  final Indicator<T> mdiS;
-
-  @override
-  T calculate(int index) {
-    if(index<1){
-      return createResult(
-          index: index,
-          quote:0);
-    }
-    return createResult(
-      index: index,
-      quote: (1 - period) * (index < 1 ? 0 : getValue(index - 1).quote) +
-          period *
-              (pdiS.getValue(index).quote - mdiS.getValue(index).quote).abs() /
-              (pdiS.getValue(index).quote + mdiS.getValue(index).quote));
+    return createResult(index: index, quote: sum/k);
   }
 }
--- packages/deriv_technical_analysis/test/indicators/calculations/vma_indicator_test.dart
+++ packages/deriv_technical_analysis/test/indicators/calculations/vma_indicator_test.dart
@@ -1,3 +1,4 @@
+import 'package:deriv_technical_analysis/deriv_technical_analysis.dart';
 import 'package:deriv_technical_analysis/src/indicators/calculations/helper_indicators/close_value_inidicator.dart';
 import 'package:deriv_technical_analysis/src/indicators/calculations/sma_indicator.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -10,39 +11,55 @@ void main() {
 
     setUpAll(() {
       ticks = const <MockTick>[
-        MockTick(epoch: 10, quote: 1),
-        MockTick(epoch: 11, quote: 2),
-        MockTick(epoch: 12, quote: 3),
-        MockTick(epoch: 13, quote: 4),
-        MockTick(epoch: 14, quote: 3),
-        MockTick(epoch: 15, quote: 4),
-        MockTick(epoch: 16, quote: 5),
-        MockTick(epoch: 17, quote: 4),
-        MockTick(epoch: 18, quote: 3),
-        MockTick(epoch: 19, quote: 3),
-        MockTick(epoch: 20, quote: 4),
-        MockTick(epoch: 21, quote: 3),
-        MockTick(epoch: 22, quote: 2),
+        MockTick(epoch: 0, quote: 84.154),
+        MockTick(epoch: 1, quote: 84.150),
+        MockTick(epoch: 2, quote: 84.150),
+        MockTick(epoch: 3, quote: 84.151),
+        MockTick(epoch: 4, quote: 84.148),
+        MockTick(epoch: 5, quote: 84.143),
+        MockTick(epoch: 6, quote: 84.136),
+        MockTick(epoch: 7, quote: 84.141),
+        MockTick(epoch: 8, quote: 84.135),
+        MockTick(epoch: 9, quote: 84.137),
+        MockTick(epoch: 10, quote: 84.135),
+        MockTick(epoch: 11, quote: 84.146),
+        MockTick(epoch: 12, quote: 84.142),
+        MockTick(epoch: 13, quote: 84.140),
+        MockTick(epoch: 14, quote: 84.141),
+        MockTick(epoch: 15, quote: 84.122),
+        MockTick(epoch: 16, quote: 84.121),
+        MockTick(epoch: 17, quote: 84.124),
+        MockTick(epoch: 18, quote: 84.128),
+        MockTick(epoch: 19, quote: 84.117),
+        MockTick(epoch: 20, quote: 84.122),
       ];
     });
 
     test('SMAIndicator calculates the correct results', () {
-      final SMAIndicator<MockResult> smaIndicator = SMAIndicator<MockResult>(
+      // final PdmS<MockResult> smaIndicator = PdmS<MockResult>(
+      //     CloseValueIndicator<MockResult>(MockInput(ticks)), 1/3)..calculateValues();
+      // final MdmS<MockResult> m = MdmS<MockResult>(
+      //     CloseValueIndicator<MockResult>(MockInput(ticks)), 1/3)..calculateValues();
+      // final MdiS<MockResult> s = MdiS<MockResult>(
+      //     CloseValueIndicator<MockResult>(MockInput(ticks)), 1/3)..calculateValues();
+      // final IS<MockResult> ss = IS<MockResult>(
+      //     CloseValueIndicator<MockResult>(MockInput(ticks)), 1/3)..calculateValues();
+      final VMAIndicator<MockResult> v = VMAIndicator<MockResult>(
           CloseValueIndicator<MockResult>(MockInput(ticks)), 3);
-
-      expect(1, smaIndicator.getValue(0).quote);
-      expect(1.5, smaIndicator.getValue(1).quote);
-      expect(2, smaIndicator.getValue(2).quote);
-      expect(3, smaIndicator.getValue(3).quote);
-      expect(10 / 3, smaIndicator.getValue(4).quote);
-      expect(11 / 3, smaIndicator.getValue(5).quote);
-      expect(4, smaIndicator.getValue(6).quote);
-      expect(13 / 3, smaIndicator.getValue(7).quote);
-      expect(4, smaIndicator.getValue(8).quote);
-      expect(10 / 3, smaIndicator.getValue(9).quote);
-      expect(10 / 3, smaIndicator.getValue(10).quote);
-      expect(10 / 3, smaIndicator.getValue(11).quote);
-      expect(3, smaIndicator.getValue(12).quote);
+      print(v.getValue(19).quote);
+      expect(v.getValue(19).quote,84.128);
+      // expect(1.5, smaIndicator.getValue(1).quote);
+      // expect(2, smaIndicator.getValue(2).quote);
+      // expect(3, smaIndicator.getValue(3).quote);
+      // expect(10 / 3, smaIndicator.getValue(4).quote);
+      // expect(11 / 3, smaIndicator.getValue(5).quote);
+      // expect(4, smaIndicator.getValue(6).quote);
+      // expect(13 / 3, smaIndicator.getValue(7).quote);
+      // expect(4, smaIndicator.getValue(8).quote);
+      // expect(10 / 3, smaIndicator.getValue(9).quote);
+      // expect(10 / 3, smaIndicator.getValue(10).quote);
+      // expect(10 / 3, smaIndicator.getValue(11).quote);
+      // expect(3, smaIndicator.getValue(12).quote);
     });
   });
 }
