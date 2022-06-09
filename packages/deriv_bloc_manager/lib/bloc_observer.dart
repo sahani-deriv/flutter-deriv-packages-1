58bd947295aba7d95e26a7147b2d9fa96f41ec44
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-06-09 09:45:34 +0800

    hamed/refactor_cubit_observer (#132)
    - refactor cubit observer

--- packages/deriv_bloc_manager/lib/bloc_observer.dart
+++ packages/deriv_bloc_manager/lib/bloc_observer.dart
@@ -4,10 +4,39 @@ import 'package:bloc/bloc.dart';
 
 /// An observer for blocs and cubits state change
 class CubitObserver extends BlocObserver {
+  /// Initializes cubit observer.
+  CubitObserver({required this.enableChangeLog});
+
+  /// Enable or disable the change logs.
+  final bool enableChangeLog;
+
+  @override
+  void onCreate(BlocBase<dynamic> bloc) {
+    super.onCreate(bloc);
+
+    logger.log('Bloc created: ${bloc.runtimeType}');
+  }
+
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
-    logger.log('${bloc.runtimeType} $change');
+    if (enableChangeLog) {
+      logger.log('${bloc.runtimeType}: $change');
+    }
 
     super.onChange(bloc, change);
+  }
+
+  @override
+  void onClose(BlocBase<dynamic> bloc) {
+    super.onClose(bloc);
+
+    logger.log('Bloc closed: ${bloc.runtimeType}');
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    logger.log('Bloc error: ${bloc.runtimeType}\n$error\n$stackTrace');
+
+    super.onError(bloc, error, stackTrace);
   }
 }
