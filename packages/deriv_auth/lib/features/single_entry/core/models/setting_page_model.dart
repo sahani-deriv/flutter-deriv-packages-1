11c8aca2042eb0ab86f3cd462927b1422df7471b
Ahrar <ahrar@deriv.com>
2024-02-13 13:51:14 +0800

    chore: setting page fist attempt

---
+++ packages/deriv_auth/lib/features/single_entry/core/models/setting_page_model.dart
@@ -0,0 +1,8 @@
+/// Setting page model
+class SettingPageModel {
+  /// Setting page App label
+  final String appLabel;
+
+  final String appId;
+  final String endpoint;
+}
--- packages/deriv_auth/lib/features/single_entry/features/settings/pages/settings_page.dart
+++ packages/deriv_auth/lib/features/single_entry/features/settings/pages/settings_page.dart
@@ -1,8 +1,12 @@
-import 'dart:developer' as logger;
 import 'package:deriv_auth/deriv_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_deriv_api/services/connection/api_manager/connection_information.dart';
+import 'package:flutter_deriv_api/state/connection/connection_cubit.dart';
 
+/// Setting page
 class SettingsPage extends StatefulWidget {
+  /// Constructor for setting page
   const SettingsPage({super.key});
 
   @override
@@ -12,17 +16,46 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) => DerivSettingLayout(
-        updateFlavorConfigs: _updateFlavorConfig(),
-        appLabel: 'appLabel',
+        updateFlavorConfigs: _updateFlavorConfigs(),
+        appLabel: 'blanc',
         saveValues: _saveValues,
+        appId: PrefService.getString('appId') ?? defaultAppId,
+        endpoint: PrefService.getString('endpoint') ?? defaultEndpoint,
       );
-}
+  Future<void> _saveValues({
+    required String endpoint,
+    required String appId,
+  }) async {
+    await PrefService.setString('endpoint', endpoint);
+    await PrefService.setString('appId', appId);
+  }
 
-_saveValues({required String appId, required String endpoint}) {
-  logger.log('appId is $appId');
-  logger.log('endpoint is $endpoint');
-}
+  Future<void> _updateFlavorConfigs() async {
+    final authEndpoint = generateEndpointUrl(
+          endpoint: PrefService.getString('endpoint'),
+          isAuthUrl: true,
+        ) ??
+        defaultAuthEndpoint;
 
-Future<void> _updateFlavorConfig() async {
-  logger.log('Flavor CONFIG is updated');
+    final endpoint =
+        generateEndpointUrl(endpoint: PrefService.getString('endpoint')) ??
+            defaultEndpoint;
+
+    FlavorConfig.instance.copyWith(
+      FlavorValues(
+        endPoint: endpoint,
+        authEndPoint: authEndpoint,
+        appId: PrefService.getString('appId')!,
+      ),
+    );
+
+    await BlocProvider.of<ConnectionCubit>(context).reconnect(
+      connectionInformation: ConnectionInformation(
+        appId: FlavorConfig.instance.flavorValues.appId,
+        brand: 'deriv',
+        endpoint: FlavorConfig.instance.flavorValues.endPoint!,
+        authEndpoint: FlavorConfig.instance.flavorValues.authEndPoint!,
+      ),
+    );
+  }
 }
