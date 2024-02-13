b0eaafe6294ee2629af12c6234758b92676e4c7c
behnam-deriv <133759298+behnam-deriv@users.noreply.github.com>
2024-02-13 10:20:54 +0800

    feat: add grouped_list_view (#445)

--- packages/deriv_ui/lib/components/components.dart
+++ packages/deriv_ui/lib/components/components.dart
@@ -1,3 +1,4 @@
 export 'banner/banner.dart';
 export 'date_range_picker/date_range_picker.dart';
 export 'expandable_bottom_sheet/expandable_bottom_sheet.dart';
+export 'grouped_list_view/grouped_list_view.dart';
