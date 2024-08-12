025e145799e508db461b64a7535269946c9d7370
Reza <94842463+Reza-deriv@users.noreply.github.com>
2024-08-12 17:35:42 +0800

    fix(deriv_auth): focus node issue in BaseTextField (#765)

--- packages/deriv_ui/lib/widgets/base_text_field.dart
+++ packages/deriv_ui/lib/widgets/base_text_field.dart
@@ -152,7 +152,9 @@ class _BaseTextFieldState extends State<BaseTextField> {
   @override
   void dispose() {
     _focusNode.removeListener(_onFocusChange);
-    _focusNode.dispose();
+    if (widget.focusNode == null) {
+      _focusNode.dispose();
+    }
     super.dispose();
   }
 
