5cf2429e959d9fa7093999bb4c20daf973cf8920
Ilya <106583608+ilya-deriv@users.noreply.github.com>
2024-10-22 13:36:43 +0300

    feat(deriv_mobile_chart_wrapper): [DRGO-75] Add_bottom_sheet_landing_logic (#841)
    Co-authored-by: Ali Akbar <116874053+aliakbar-deriv@users.noreply.github.com>

--- packages/deriv_mobile_chart_wrapper/lib/src/mobile_tools_ui/drawing_tools/drawing_tools_selector.dart
+++ packages/deriv_mobile_chart_wrapper/lib/src/mobile_tools_ui/drawing_tools/drawing_tools_selector.dart
@@ -49,6 +49,12 @@ class _DrawingToolsSelectorState extends State<DrawingToolsSelector>
       context,
     );
     _drawingTools = getDrawingToolsList(context);
+
+    // Only change the tab index if the active drawing tools list is empty and 
+    // the current tab is not the same 1st index tab.
+    if (drawingToolsRepo.items.isEmpty && _tabController.index != 1) {
+      _tabController.index = 1;
+    }
   }
 
   @override
